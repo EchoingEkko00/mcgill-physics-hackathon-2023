9559974a1cdda1fb429ae02fa0b6101f30f8138c
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2023-11-06 01:51:48 -0500

    ajout de docs

---
+++ docs/precache-manifest.be69b7ec2be8b88ee98e5cc06402d5b6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3d0369f2dad15830583904ae92db52",
+    "url": "/mcgill-physics-hackathon-2023/index.html"
+  },
+  {
+    "revision": "7e4d22971aaec0346e84",
+    "url": "/mcgill-physics-hackathon-2023/static/css/main.27448951.chunk.css"
+  },
+  {
+    "revision": "e9bd8eabb94626a2305a",
+    "url": "/mcgill-physics-hackathon-2023/static/js/2.0379d2eb.chunk.js"
+  },
+  {
+    "revision": "2c20a8e3080d7847282ad31270a5cac1",
+    "url": "/mcgill-physics-hackathon-2023/static/js/2.0379d2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a85251e1d176eb2c21d",
+    "url": "/mcgill-physics-hackathon-2023/static/js/3.9aeac87d.chunk.js"
+  },
+  {
+    "revision": "7e4d22971aaec0346e84",
+    "url": "/mcgill-physics-hackathon-2023/static/js/main.c3f51a1b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/mcgill-physics-hackathon-2023/static/js/main.c3f51a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e8b23c8a654bb7f541",
+    "url": "/mcgill-physics-hackathon-2023/static/js/runtime-main.bf12fa85.js"
+  },
+  {
+    "revision": "06e26c980190840655cb775269260d0c",
+    "url": "/mcgill-physics-hackathon-2023/static/media/earth-transparent-png-9.06e26c98.png"
+  },
+  {
+    "revision": "6721c541fcc9373dfc5c26faf36e1b52",
+    "url": "/mcgill-physics-hackathon-2023/static/media/full-moon-transparent-background-7.6721c541.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mcgill-physics-hackathon-2023/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b8de18c84d29fe20c145863d828f653c",
+    "url": "/mcgill-physics-hackathon-2023/static/media/rocket.b8de18c8.png"
+  }
+]);
